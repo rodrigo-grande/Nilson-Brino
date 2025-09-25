de70054d4a2f9e24da2a1a4ea8748a731d43df8d
rodrigo-grande <rodrigo.grande@escola.pr.gov.br>
2025-09-25 12:09:03 -0300

    a

---
+++ flakes.js
@@ -0,0 +1,21 @@
+const container = document.getElementById('fotos');
+    const imagens = container.getElementsByTagName('img');
+
+    const fotosOriginais = ["flakes 2.webp", "flakes 3.webp", "flakes.webp"];
+    const novasFotos = ["poliana.jpg", "kaua.jpg", "celso.webp"];
+
+    let trocou = false;
+
+    container.addEventListener('click', () => {
+        if (!trocou) {
+            for (let i = 0; i < imagens.length; i++) {
+                imagens[i].src = novasFotos[i];
+            }
+            trocou = true;
+        } else {
+            for (let i = 0; i < imagens.length; i++) {
+                imagens[i].src = fotosOriginais[i];
+            }
+            trocou = false;
+        }
+    });
