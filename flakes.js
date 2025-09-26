9cf96c5f9bd2ff1683f68eb4ceafa61d2f1b0b4a
rodrigo-grande <rodrigo.grande@escola.pr.gov.br>
2025-09-26 11:51:35 -0300

    a

--- flakes.js
+++ flakes.js
@@ -1,8 +1,12 @@
 const container = document.getElementById('fotos');
     const imagens = container.getElementsByTagName('img');
 
-    const fotosOriginais = ["flakes 2.webp", "flakes 3.webp", "flakes.webp"];
+    const fotosOriginais = ["flakes 2.webp", "flakes 3.webp", "flakes.webp","flakescalvo.jpeg","falkes.jpeg","flakescalvo.jpeg"];
     const novasFotos = ["dioni.jpg", "dioni.jpg", "dioni.jpg"];
+    
+
+
+
 
     let trocou = false;
 
